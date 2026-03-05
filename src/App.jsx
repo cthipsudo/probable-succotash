6fc5c94e05a3edc19bfce73be34c484ed5a36b27
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-04 21:44:39 -0500

    components and props practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
-import Bird from './Bird'
 import './App.css'
+import Bird from './Bird'
+import Greeter from './Greeter'
+import Die from './Die'
 
 function App() {
   return <div>
-    <Bird />
-    <Bird />
-    <Bird />
+    {/* <Greeter person="Bill" from="Oscar" />
+    <Greeter person="Dennis" from="Oscar" />
+    <Greeter person="Okarun" from="Momo" /> */}
+    <Die numSides={20}/>
+    <Die numSides={6}/>
+    <Die numSides={8}/>
   </div>
 }
 
