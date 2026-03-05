f31d4eecb627e5fcf769e18723c2c209812750fd
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-04 23:17:22 -0500

    conditionals react

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Bird from './Bird'
 import Greeter from './Greeter'
 import Die from './Die'
 import ListPicker from './ListPicker'
+import DoubleDice from './DoubleDice'
 
 function App() {
   return <div>
@@ -13,8 +14,9 @@ function App() {
     {/* <Die numSides={20}/>
     <Die />
     <Die numSides={8}/> */}
-    <ListPicker values={[1,2,3]}/>
-    <ListPicker values={["a", "b", "c"]}/>
+    {/* <ListPicker values={[1,2,3]}/>
+    <ListPicker values={["a", "b", "c"]}/> */}
+    <DoubleDice />
   </div>
 }
 
